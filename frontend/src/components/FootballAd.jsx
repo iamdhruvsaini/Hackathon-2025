50ca151ab53cb2c50c03f136c7c71f730e89e0c3
dhruv <dhruvsaini230@gmail.com>
2025-02-11 23:02:54 +0530

    fix/UI modified

--- frontend/src/components/FootballAd.jsx
+++ frontend/src/components/FootballAd.jsx
@@ -4,20 +4,23 @@ import league from "../assets/league.gif";
 const FootballAd = () => {
   return (
     <section>
-      <div  className="mx-auto grid max-w-screen-xl rounded-lg p-4 md:p-8 lg:grid-cols-12 lg:gap-8 lg:p-16 xl:gap-16">
-        <div  className="lg:col-span-5 lg:mt-0">
-            <img src={league} alt="" className="w-[60%] sm:w-[40%] lg:w-full rounded-xl mx-auto" />
+      <div className="mx-auto grid max-w-screen-xl rounded-lg p-4 md:p-8 lg:grid-cols-12 lg:gap-8 lg:p-16 xl:gap-16">
+        <div className="lg:col-span-5">
+          <img
+            src={league}
+            alt=""
+            className="w-[60%] sm:w-[40%] lg:w-full rounded-xl mx-auto"
+          />
         </div>
-        <div  className="me-auto place-self-center lg:col-span-7">
-          <h1  className="mb-3 text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-4xl">
-            Save $500 today on your purchase <br />
-            of a new iMac computer.
+        <div className="me-auto place-self-center lg:col-span-7">
+          <h1 className="mb-3 text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-4xl">
+            Optimize your squad today and <br />
+            build the best football team now!
           </h1>
-          <p  className="mb-6 text-gray-500 dark:text-gray-400">
-            Reserve your new Apple iMac 27” today and enjoy exclusive savings
-            with qualified activation. Pre-order now to secure your discount.
+          <p className="text-gray-500 dark:text-gray-400">
+            Reserve your dream football squad today and get exclusive insights
+            with advanced analytics. Optimize now to build your best team!
           </p>
-        
         </div>
       </div>
     </section>
